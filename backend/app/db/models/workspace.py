3d16d0c0a35bc9690b46be4012db6d9b1fc936bc
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-02-09 19:23:09 +0530

    Create workspace.py

---
+++ backend/app/db/models/workspace.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from app.db.base import Base
+
+class Workspace(Base):
+    __tablename__ = "workspaces"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String, default="")
