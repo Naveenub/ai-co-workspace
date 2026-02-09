590e0c4f1256dcb93721ec4f1efc94512e409063
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-02-09 21:17:24 +0530

    Create ChatPanel.tsx

---
+++ frontend/src/components/ChatPanel.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatPanel = () => {
+  return (
+    <div className="flex-1 bg-gray-50 p-4 overflow-y-auto">
+      <p>Chat messages will appear here...</p>
+    </div>
+  );
+};
+
+export default ChatPanel;
