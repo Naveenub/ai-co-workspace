732d77cf4611f516c7faafdc1ba32a55b1b5ccc4
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-02-09 19:26:44 +0530

    Update chat.py

--- backend/app/db/models/chat.py
+++ backend/app/db/models/chat.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, Integer, String
 from app.db.base import Base
 
-class chat(Base):
-    __tablename__ = "chat"
+class Chat(Base):
+    __tablename__ = "chats"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
